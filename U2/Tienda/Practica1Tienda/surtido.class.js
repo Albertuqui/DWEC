923605ced1454161761eb435087835c79442a4bb
Alberto Hernández <albertohernandezbaggetto@gmail.com>
2017-10-27 18:39:14 +0200

    subo

--- U2/Tienda/Practica1Tienda/surtido.class.js
+++ U2/Tienda/Practica1Tienda/surtido.class.js
@@ -1,9 +1,47 @@
 function Surtido(productos){
   this.productos=productos;
+  this.dibujarSurtido =function(produ){
+    cuerpo.innerHTML = "";
+    var str = "";
+    if(produ == undefined){
+      for (var i = 0; i < this.productos.length; i++) {
+        str += "<div>";
+        str += "<img src='" + this.productos[i].foto + "' alt= '";
+        str += this.productos[i].nombre + "'>";
+        str += "<p> Producto: " + this.productos[i].nombre + "</p>";
+        str += "<p> " + this.productos[i].descripcion + "</p>";
+        str += "<p><b> " + this.productos[i].precio + " €</b></p>";
+        str += "</div>";
+}
+    }
+    else{
+      for (var i = 0; i < produ.length; i++) {
+        str += "<div>";
+        str += "<img src='" + produ[i].foto + "' alt= '";
+        str += produ[i].nombre + "'>";
+        str += "<p> Producto: " + produ[i].nombre + "</p>";
+        str += "<p> " + produ[i].descripcion + "</p>";
+        str += "<p><b> " + produ[i].precio + " €</b></p>";
+        str += "</div>";
+}
+    }
+    cuerpo.innerHTML= str;
+  }
+
   this.ordenPrecioMas = function (){
   this.productos.sort(function(p1,p2){
       return p1.precio-p2.precio;
-    });
-  }
+    });}
 
-}
+    this.ordenPrecioMenos = function (){
+    this.productos.sort(function(p1,p2){
+        return p2.precio-p1.precio;
+      });}
+
+      this.filtrarVegano = function (){
+        var v = this.productos.filter(function(num) {
+          return num.vegano;
+  });
+return v; }
+
+    }
