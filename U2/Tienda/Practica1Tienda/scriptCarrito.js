e63ae1052762d1bc57e3c7dca637416b5dbf28b4
Albertuqui <albertohernandezbaggetto@gmail.com>
2017-11-01 23:34:59 +0100

    subir

--- U2/Tienda/Practica1Tienda/scriptCarrito.js
+++ U2/Tienda/Practica1Tienda/scriptCarrito.js
@@ -1,4 +1,4 @@
- if(navigator.cookieEnabled=="false"){
+ if(navigator.cookieEnabled==false){
    alert("No has habilitado las cookies");
  }
 
@@ -11,18 +11,18 @@ function eliminar(){
   usuario1.eliminar();
 }
 
-function hacerPedido(){
+function hacerPedido(productos){
   if (productos>0) {
-    function Confirmar() {
-        var response = confirm("Do you want more money?");
+        var response = confirm("¿Aceptas las condiciones de pago?");
         if(response) {
-            console.log("Get a better job");
-        } else {
-            console.log("Are you sure? OK...");
+          var v = window.open("","","height=200,width=200");
+          v.location.assign("pedidoOK.html");
+        }
+         else {
+            alert("No has aceptado las condiciones. No puedes hacer el pedido.");
         }
     }
-    var v = window.open("","","height=200,width=200");
-    v.location.assign("pedidoOK.html");
-
+  else{
+    alert("No puedes realizar pedidos porque no hay productos");
   }
 }
